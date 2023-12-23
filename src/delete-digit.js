85d7d48a4b9e9cb03c02e8a7d8e14e992a6f67d8
unknown <nexloder05@gmail.com>
2023-12-23 20:47:10 +0200

    feat: implement some of algorithmic tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,8 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  let arr = [];
+  const temp = n.toString();
+  for (let i = 0; i < temp.length; i++) {
+    let temp2 = temp.slice(0, i) + temp.slice(i + 1);
+    arr.push(Number(temp2));
+  }
+  return Math.max(...arr);
   // remove line with error and write your code here
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let arrCopy = Array.from(arr);
+  const discardPrev = '--discard-prev';
+  const doublePrev = '--double-prev';
+  const discardNext = '--discard-next';
+  const doubleNext = '--double-next';
+  for (let i = 0; i < arrCopy.length; i++) {
+    if (arrCopy[i] === doublePrev && i !== 0) {
+      arrCopy[i] = arrCopy[i - 1];
+    } else if (arrCopy[i] === doublePrev && i === 0) {
+      arrCopy.splice(i, 1);
+    } else if (arrCopy[i] === discardPrev && i !== 0) {
+      arrCopy.splice(i - 1, 2);
+    } else if (arrCopy[i] === discardPrev && i === 0) {
+      arrCopy.splice(i, 1);
+    } else if (arrCopy[i] === discardNext && i !== arrCopy.length - 1) {
+      arrCopy.splice(i, 2);
+      if (arrCopy[i] === doublePrev || arrCopy[i] === discardPrev) {
+        arrCopy.splice(i, 1);
+      }
+    } else if (arrCopy[i] === doubleNext && i !== arrCopy.length - 1) {
+      arrCopy[i] = arrCopy[i + 1];
+    } else if (arrCopy[i] === doubleNext && i === arrCopy.length - 1) {
+      arrCopy.splice(i, 1);
+    } else if (arrCopy[i] === discardNext && i === arrCopy.length - 1) {
+      arrCopy.splice(i, 1);
+    }
+  }
+  return arrCopy;
   // remove line with error and write your code here
 }
 
