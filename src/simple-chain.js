241658f5f93859ddf00cd68822929e98a6993038
unknown <nexloder05@gmail.com>
2023-12-25 10:33:05 +0200

    feat: implement some of algorithmic tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,55 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.arr.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    value !== null ? this.arr.push(value.toString()) : this.arr.push('null');
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position !== 'number' ||
+      !Number.isInteger(position) ||
+      position < 1 ||
+      position > this.arr.length
+    ) {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.arr.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    let finalStr = '';
+    if (this.arr.length > 0) {
+      this.arr.map((element, index) => {
+        if (this.arr.length > 1 && index !== this.arr.length - 1) {
+          finalStr = finalStr.concat(`( ${element} )~~`);
+        } else {
+          finalStr = finalStr.concat(`( ${element} )`);
+        }
+      });
+    }
+    this.arr.splice(0, this.arr.length);
+    return finalStr;
     // remove line with error and write your code here
-  }
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
